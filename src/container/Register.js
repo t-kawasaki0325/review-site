cafa4ff66d4d6c4beb3d4e58f8c30641f2d6a6bb
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-02-03 12:07:23 +0900

    ユーザー登録のviewを作成

--- src/container/Register.js
+++ src/container/Register.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import { loginWithEmail, loginWithGoogle } from '../module/Authentication';
 
 class Register extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
   render() {
-    return <p>test</p>;
+    return (
+      <div>
+        <input
+          type="email"
+          value={this.state.email}
+          onChange={event => this.setState({ email: event.target.value })}
+        />
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={event => this.setState({ password: event.target.value })}
+        />
+        <button
+          onClick={() => loginWithEmail(this.state.email, this.state.password)}
+        >
+          login with email
+        </button>
+        <button onClick={() => loginWithGoogle()}>login with google</button>
+      </div>
+    );
   }
 }
 
