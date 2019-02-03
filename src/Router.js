96a29e420efafe2417d11763070ea188ea303c91
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-02-03 16:23:11 +0900

    loginページを追加

--- src/Router.js
+++ src/Router.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import { Register } from './container';
+import { Register, Login } from './container';
 
 const AppRouter = () => {
   return (
     <Router>
       <Switch>
-        <Route path="/" exact component={Register} />
+        <Route path="/" exact component={Login} />
+        <Route path="/register" exact component={Register} />
       </Switch>
     </Router>
   );
---
+++ src/container/Login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import Authentication from '../module';
+
+class Login extends Component {
+  state = {
+    email: '',
+    password: '',
+  };
+
+  componentDidMount() {
+    Authentication.completeLoginWithGoogle();
+  }
+
+  render() {
+    return (
+      <div>
+        <input
+          type="email"
+          value={this.state.email}
+          onChange={event => this.setState({ email: event.target.value })}
+        />
+        <input
+          type="password"
+          value={this.state.password}
+          onChange={event => this.setState({ password: event.target.value })}
+        />
+        <button
+          onClick={() =>
+            Authentication.loginWithEmail(this.state.email, this.state.password)
+          }
+        >
+          login with email
+        </button>
+        <button onClick={() => Authentication.loginWithGoogle()}>
+          login with google
+        </button>
+      </div>
+    );
+  }
+}
+
+export default Login;
