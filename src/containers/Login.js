371a8c61059c56964125638532ffb5eb7d457f59
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-02-03 20:29:17 +0900

    loginページのレイアウトを調整

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,44 +1,124 @@
 import React, { Component } from 'react';
 import Authentication from '../modules';
-import Header from '../components';
+import PropTypes from 'prop-types';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import FormControl from '@material-ui/core/FormControl';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import withStyles from '@material-ui/core/styles/withStyles';
+
+const styles = theme => ({
+  main: {
+    width: 'auto',
+    display: 'block',
+    marginLeft: theme.spacing.unit * 3,
+    marginRight: theme.spacing.unit * 3,
+    [theme.breakpoints.up(400 + theme.spacing.unit * 3 * 2)]: {
+      width: 400,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 8,
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    padding: `${theme.spacing.unit * 2}px ${theme.spacing.unit * 3}px ${theme
+      .spacing.unit * 3}px`,
+  },
+  avatar: {
+    margin: theme.spacing.unit,
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: '100%',
+    marginTop: theme.spacing.unit,
+  },
+  submit: {
+    marginTop: theme.spacing.unit * 3,
+  },
+});
 
 class Login extends Component {
   state = {
     email: '',
     password: '',
   };
-
-  componentDidMount() {
-    Authentication.completeLoginWithGoogle();
-  }
-
   render() {
+    const { classes } = this.props;
     return (
-      <div>
-        <Header />
-        <input
-          type="email"
-          value={this.state.email}
-          onChange={event => this.setState({ email: event.target.value })}
-        />
-        <input
-          type="password"
-          value={this.state.password}
-          onChange={event => this.setState({ password: event.target.value })}
-        />
-        <button
-          onClick={() =>
-            Authentication.loginWithEmail(this.state.email, this.state.password)
-          }
-        >
-          login with email
-        </button>
-        <button onClick={() => Authentication.loginWithGoogle()}>
-          login with google
-        </button>
-      </div>
+      <main className={classes.main}>
+        <CssBaseline />
+        <Paper className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            ログイン
+          </Typography>
+          <div className={classes.form}>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="email">メールアドレス</InputLabel>
+              <Input
+                id="email"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={this.state.email}
+                onChange={event => this.setState({ email: event.target.value })}
+              />
+            </FormControl>
+            <FormControl margin="normal" required fullWidth>
+              <InputLabel htmlFor="password">パスワード</InputLabel>
+              <Input
+                name="password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={this.state.password}
+                onChange={event =>
+                  this.setState({ password: event.target.value })
+                }
+              />
+            </FormControl>
+            <Button
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+              onClick={() =>
+                Authentication.loginWithEmail(
+                  this.state.email,
+                  this.state.password
+                )
+              }
+            >
+              ログイン
+            </Button>
+          </div>
+          <Button
+            fullWidth
+            variant="contained"
+            color="default"
+            className={classes.submit}
+            onClick={() => Authentication.loginWithGoogle()}
+          >
+            Googleでログイン
+          </Button>
+        </Paper>
+      </main>
     );
   }
 }
 
-export default Login;
+Login.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Login);
