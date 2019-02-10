023610e52515291c1bd95db6eed955bac3e6217a
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-02-10 17:16:56 +0900

    mypageのviewを作成

--- src/containers/Mypage.js
+++ src/containers/Mypage.js
@@ -1,9 +1,79 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import withStyles from '@material-ui/core/styles/withStyles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+
+import { MemberInfo, Header } from '../components';
+
+const styles = theme => ({
+  layout: {
+    width: 'auto',
+    marginLeft: theme.spacing.unit * 2,
+    marginRight: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 2 * 2)]: {
+      width: 600,
+      marginLeft: 'auto',
+      marginRight: 'auto',
+    },
+  },
+  paper: {
+    marginTop: theme.spacing.unit * 3,
+    marginBottom: theme.spacing.unit * 3,
+    padding: theme.spacing.unit * 2,
+    [theme.breakpoints.up(600 + theme.spacing.unit * 3 * 2)]: {
+      marginTop: theme.spacing.unit * 6,
+      marginBottom: theme.spacing.unit * 6,
+      padding: theme.spacing.unit * 3,
+    },
+  },
+  title: {
+    margin: theme.spacing.unit * 2,
+  },
+  text: {
+    textAlign: 'center',
+    fontSize: 18,
+  },
+  button: {
+    textAlign: 'center',
+  },
+  google: {
+    marginTop: theme.spacing.unit * 2,
+  },
+  icon: {
+    height: 30,
+    marginRight: 10,
+  },
+});
 
 class Mypage extends Component {
   render() {
-    return <p>mypage</p>;
+    const { classes, history } = this.props;
+    return (
+      <React.Fragment>
+        <Header history={history} />
+        <CssBaseline />
+        <main className={classes.layout}>
+          <Paper className={classes.paper}>
+            <Typography
+              component="h1"
+              variant="h4"
+              align="center"
+              className={classes.title}
+            >
+              マイページ編集
+            </Typography>
+            <MemberInfo history={history} />
+          </Paper>
+        </main>
+      </React.Fragment>
+    );
   }
 }
 
-export default Mypage;
+Mypage.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Mypage);
