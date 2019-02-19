c4b7a239b7f7355c9616a536c0eaea165fedce1b
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-02-19 22:32:21 +0900

    reduxの削除

--- src/index.js
+++ src/index.js
@@ -1,18 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import Router from './Router';
-import createStore from './modules/createStore';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore();
-
-ReactDOM.render(
-  <Provider store={store}>
-    <Router />
-  </Provider>,
-  document.getElementById('root')
-);
+ReactDOM.render(<Router />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
