493d29fa9db818de649d2f71e92c373fee8c8f6f
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-03-09 12:42:57 +0900

    pointの設定値を追加

--- src/config/index.js
+++ src/config/index.js
@@ -1,8 +1,5 @@
-import COMPANY from './company';
-
 export { default as PATH } from './path';
 export { default as COMPANY } from './company';
 export { default as SAAS } from './saas';
 export { default as REVIEW } from './review';
-
-export default COMPANY;
+export { default as POINT } from './point';
