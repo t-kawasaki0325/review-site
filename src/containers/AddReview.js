8f5d02cede072ba83a0cc5c7f08ff006c4fdff91
Takahiro Kawasaki <takahiro.kawasaki0325@gmail.com>
2019-03-21 10:59:56 +0900

    レビュー仕様変更に伴う削除

--- src/containers/AddReview.js
+++ src/containers/AddReview.js
@@ -5,7 +5,6 @@ import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
-import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
@@ -64,24 +63,41 @@ const styles = theme => ({
 class AddReview extends Component {
   state = {
     info: {
+      // review
       recommendation: '',
-      sales: '',
-      support: '',
-      utilization: '',
+      title: '',
+      good: '',
+      bad: '',
+      // basic
+      isAdmin: '',
+      contractStatus: '',
+      contractDate: '',
+      price: '',
+      priceOption: '',
+      priceSatisfaction: '',
+      licenseNum: '',
+      isContinue: '',
+      reasonNotContinue: '',
       satisfaction: '',
-      opportunity: '',
+      //untilAdopt
+      isParticipant: '',
       firstContact: '',
+      reasonFirstContact: '',
       considerationReason: '',
-      considerationPeriod: '',
       otherSaas: '',
+      considerationPeriod: '',
+      sales: '',
       isDiscounted: '',
       discountRate: '',
-      onboadingPeriod: '',
-      price: '',
-      period: '',
+      decision: '',
+      // adoptiong
       onboadingSystem: '',
-      fromNow: '',
-      content: '',
+      onboadingPeriod: '',
+      onboadingSatisfaction: '',
+      isOperationParticipant: '',
+      support: '',
+      supportSatisfaction: '',
+      utilization: '',
     },
     uid: '',
     saasId: '',
@@ -126,134 +142,37 @@ class AddReview extends Component {
 
   render() {
     const { classes, history } = this.props;
+    const { info, name, uid, loading } = this.state;
 
     const reviewCell = [
       {
-        label: SAAS.RADAR.sales,
-        value: this.state.info.sales,
-        key: 'sales',
-        list: REVIEW.SATISFACTION_LEVEL,
-      },
-      {
-        label: SAAS.RADAR.support,
-        value: this.state.info.support,
-        key: 'support',
-        list: REVIEW.SATISFACTION_LEVEL,
-      },
-      {
-        label: SAAS.RADAR.utilization,
-        value: this.state.info.utilization,
-        key: 'utilization',
-        list: REVIEW.SATISFACTION_LEVEL,
-      },
-      {
         label: SAAS.RADAR.recommendation,
-        value: this.state.info.recommendation,
+        value: info.recommendation,
         key: 'recommendation',
         list: REVIEW.RECOMMENDATION_LEVEL,
       },
-      {
-        label: SAAS.RADAR.satisfaction,
-        value: this.state.info.satisfaction,
-        key: 'satisfaction',
-        list: REVIEW.SATISFACTION_LEVEL,
-      },
     ];
 
-    const untilAdopt = [
+    const reviewText = [
       {
-        label: REVIEW.UNTIL_ADOPTED_TITLE.OPPORTUNITY,
-        value: this.state.info.opportunity,
-        key: 'opportunity',
-        list: REVIEW.UNTIL_ADOPTED.OPPORTUNITY,
-      },
-      {
-        label: REVIEW.UNTIL_ADOPTED_TITLE.FIRST_CONTACT,
-        value: this.state.info.firstContact,
-        key: 'firstContact',
-        list: REVIEW.UNTIL_ADOPTED.FIRST_CONTACT,
-      },
-      {
-        label: REVIEW.UNTIL_ADOPTED_TITLE.REASON,
-        value: this.state.info.considerationReason,
-        key: 'considerationReason',
-        list: REVIEW.UNTIL_ADOPTED.REASON,
-      },
-      {
-        label: REVIEW.UNTIL_ADOPTED_TITLE.PERIOD,
-        value: this.state.info.considerationPeriod,
-        key: 'considerationPeriod',
-        list: REVIEW.UNTIL_ADOPTED.PERIOD,
-      },
-    ];
-
-    const untilAdoptText = [
-      {
-        label: REVIEW.UNTIL_ADOPTED_TITLE.OTHER_SAAS,
-        value: this.state.info.otherSaas,
-        key: 'otherSaas',
-      },
-    ];
-
-    const adopting = [
-      {
-        label: REVIEW.BEING_ADOPTED_TITLE.IS_DISCOUNTED,
-        value: this.state.info.isDiscounted,
-        key: 'isDiscounted',
-        list: REVIEW.BEING_ADOPTED.IS_DISCOUNTED,
-      },
-      {
-        label: REVIEW.BEING_ADOPTED_TITLE.DISCOUNT_RATE,
-        value: this.state.info.discountRate,
-        key: 'discountRate',
-        list: REVIEW.BEING_ADOPTED.DISCOUNT_RATE,
-      },
-      {
-        label: REVIEW.BEING_ADOPTED_TITLE.ONBOADING_PERIOD,
-        value: this.state.info.onboadingPeriod,
-        key: 'onboadingPeriod',
-        list: REVIEW.BEING_ADOPTED.ONBOADING_PERIOD,
-      },
-      {
-        label: REVIEW.BEING_ADOPTED_TITLE.PRICE,
-        value: this.state.info.price,
-        key: 'price',
-        list: REVIEW.BEING_ADOPTED.PRICE,
-      },
-      {
-        label: REVIEW.BEING_ADOPTED_TITLE.PERIOD,
-        value: this.state.info.period,
-        key: 'period',
-        list: REVIEW.BEING_ADOPTED.PERIOD,
-      },
-      {
-        label: REVIEW.BEING_ADOPTED_TITLE.FROM_NOW,
-        value: this.state.info.fromNow,
-        key: 'fromNow',
-        list: REVIEW.BEING_ADOPTED.FROM_NOW,
-      },
-    ];
-
-    const adoptingText = [
-      {
-        label: REVIEW.BEING_ADOPTED_TITLE.ONBOADING_SYSTEM,
-        value: this.state.info.onboadingSystem,
-        key: 'onboadingSystem',
+        label: 'レビューのタイトル',
+        value: info.title,
+        key: 'title',
       },
     ];
 
     return (
       <React.Fragment>
-        <Header history={history} uid={this.state.uid} />
+        <Header history={history} uid={uid} />
         <CssBaseline />
         <main className={classes.layout}>
           <div className={classes.appBarSpacer} />
           <Typography component="h1" variant="h4" className={classes.title}>
-            {this.state.name} 評価レポート
+            {name} 評価レポート
           </Typography>
           <div className={classes.container}>
             <Typography component="h1" variant="h6" gutterBottom>
-              全体的な満足度
+              全体評価レビュー
             </Typography>
             <Paper className={classes.paper}>
               <Table>
@@ -262,68 +181,17 @@ class AddReview extends Component {
                     list={reviewCell}
                     handleChange={event => this.handleChange(event)}
                   />
-                </TableBody>
-              </Table>
-            </Paper>
-          </div>
-          <div className={classes.container}>
-            <Typography component="h1" variant="h6" gutterBottom>
-              サービスを導入するまで
-            </Typography>
-            <Paper className={classes.paper}>
-              <Table>
-                <TableBody>
-                  <TableSelect
-                    list={untilAdopt}
-                    handleChange={event => this.handleChange(event)}
-                  />
                   <TableText
-                    list={untilAdoptText}
+                    list={reviewText}
                     handleChange={event => this.handleChange(event)}
                   />
                 </TableBody>
               </Table>
-            </Paper>
-          </div>
-          <div className={classes.container}>
-            <Typography component="h1" variant="h6" gutterBottom>
-              サービス導入にあたって
-            </Typography>
-            <Paper className={classes.paper}>
-              <Table>
-                <TableBody>
-                  <TableSelect
-                    list={adopting}
-                    handleChange={event => this.handleChange(event)}
-                  />
-                  <TableText
-                    list={adoptingText}
-                    handleChange={event => this.handleChange(event)}
-                  />
-                </TableBody>
-              </Table>
-            </Paper>
-          </div>
-          <div className={classes.container}>
-            <Typography component="h1" variant="h6" gutterBottom>
-              ご自由にレビューを記載してください(100字以上)
-            </Typography>
-            <Paper className={classes.paper}>
-              <TextField
-                className={classes.textArea}
-                name="content"
-                value={this.state.info.content}
-                placeholder="ご自由にレビューをお書きください"
-                onChange={event => this.handleChange(event)}
-                multiline={true}
-                rows={4}
-                rowsMax={10}
-              />
             </Paper>
           </div>
           <div className={classes.buttonWrapper}>
             <Button
-              disabled={this.state.loading}
+              disabled={loading}
               variant="contained"
               color="primary"
               onClick={() => this.confirmReview()}
@@ -331,7 +199,7 @@ class AddReview extends Component {
             >
               確認ページへ進む
             </Button>
-            {this.state.loading && (
+            {loading && (
               <CircularProgress size={24} className={classes.buttonProgress} />
             )}
           </div>
